1a15b6e614c7e40db3d0d90595aeee54fedd17d0
David Wilbanks <dwilbank@gmail.com>
2017-07-08 01:08:29 -0700

    before merge

---
+++ imports/api/email.js
@@ -0,0 +1,102 @@
+import {Meteor} from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import {Mongo} from 'meteor/mongo';
+// import {data} from '../../addresses.js';
+
+import { Email } from 'meteor/email';
+
+export const Needs = new Mongo.Collection('needs');
+//
+if (Meteor.isServer) {
+    Meteor.publish('needs', function () {
+        return Needs.find(
+            {},
+            {
+                fields: {
+                    address: 1,
+                    avatar: 1,
+                    body: 1,
+                    created: 1,
+                    email: 1,
+                    needPicture: 1,
+                    screenName: 1,
+                    urgent: 1,
+                    userId: 1,
+                    userName: 1
+                }
+            }
+        );
+    })
+}
+//
+// export const validateProfile = (userId, profileObj) => {
+//     const {address, avatar, emailVisible, fullName, screenName, phone} = profileObj;
+//     new SimpleSchema({
+//         userId: {type: String},
+//         address: {
+//             type: String, min: 4
+//         },
+//         avatar: {type: String},
+//         emailVisible: {type: Boolean},
+//         screenName: {
+//             type: String, min: 2, max: 12
+//         },
+//         fullName: {
+//             type: String, max: 30, optional: true
+//         },
+//         phone: {type: String, optional: true}
+//     })
+//         .validate({address, avatar, emailVisible, fullName, screenName, userId, phone});
+//     return true;
+// }
+
+Meteor.methods({
+    'needCreate'(msg, emailRecipients, smsRecipients){
+        console.log('------------------------------------------');
+        console.log('emailRecipients ',emailRecipients);
+        console.log('------------------------------------------');
+        // validateMessage(userId, updatesObj);
+        if (!this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+        const {address, avatar, body, email, needPicture, screenName, urgent, userId} = msg;
+        if (Meteor.isServer) {
+            console.log('------------------------------------------');
+            console.log('process.env.MAIL_URL ',process.env.MAIL_URL);
+            console.log('------------------------------------------');
+            Email.send({
+                subject: 'test for 2 emails',
+                text: body,
+                from: 'noreply@traditions90250.com',
+                to: emailRecipients
+            });
+        }
+
+        // mg.sendText('traditions90250@noreply.com', 'dwilbank@gmail.com', 'new request', body, (err) => {
+        //     console.log('------------------------------------------');
+        //     console.log('err ',err);
+        //     console.log('------------------------------------------');
+        // })
+
+        return Needs.insert(
+            {
+                address,
+                avatar,
+                body,
+                created: new Date().getTime(),
+                email,
+                needPicture,
+                screenName,
+                urgent,
+                userId
+            }
+        )
+    },
+    'needDelete'(msgId){
+        // validateMessage(userId, updatesObj);
+        if (!this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+        return Needs.remove({'_id': msgId})
+    }
+})
--- server/main.js
+++ server/main.js
@@ -4,6 +4,7 @@ import {Mongo} from 'meteor/mongo';
 import {WebApp} from 'meteor/webapp';
 
 import '../imports/api/announcements';
+import '../imports/api/email';
 import '../imports/api/messages';
 import '../imports/api/needs';
 import '../imports/api/offers';
@@ -14,6 +15,7 @@ import '../imports/startup/simpl-schema-configuration.js';
 
 Meteor.startup(() => {
     if (Meteor.settings.MAIL_URL) {
+        // process.env.MAIL_URL = "smtp://postmaster%40mg.traditions90250.com:d5baecfc568c697fa8b7499f4ae48b35@smtp.mailgun.org:587";
         process.env.MAIL_URL = "smtp://postmaster%40mg.traditions90250.com:d5baecfc568c697fa8b7499f4ae48b35@smtp.mailgun.org:587";
     }
 });
